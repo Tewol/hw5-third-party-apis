bae8a3e7b7f05ac3ade4dbe1c2f6e83b947ed77d
Hewan Redie <tewol22@gmail.com>
2021-06-22 11:51:06 -0500

    update index file and update color coding in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,43 @@
 
 
 // 1. Current Day display at the top of the calander
-var current = moment().format("MM DD, YYYY");
+var current = moment().format("MMM D, YYYY");
 $("#currentDay").text(current);
 
+
+var events = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //2. scrolling down should present a timeblocks for business hours
 
 
 
-//3. Each timeblock is color coded to indicate whether it is in the past, present, or future
+//3. Each timeblock is color coded to indicate the past, present, or future dates.
+
+var calApperance = function() {
+
+    var currentHour = moment().hour();
+
+    console.log(currentHour);
+
+     $(".row").each(function(){
+        var hourElm = parseInt ($(this).attr("id"));
+
+        if (hourElm < currentHour){
+            $(this).removeClass(["present", "future"]).addClass("past");
+
+        } else if (hourElm === currentHour){
+            $(this).removeClass(["papastst", "future"]).addClass("present");
+
+        }else{
+
+            $(this).removeClass(["past", "present"]).addClass("future");
+        }
+
+    })
+
+   
+
+};
 
 
 //4. Should be able to enter an event by clicking inside the timeblock.
@@ -20,4 +48,23 @@ $("#currentDay").text(current);
 
 
 //6. Refresing the page will save the events entered
+
+
+
+calApperance();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
