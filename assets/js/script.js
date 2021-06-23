81e8f486d00d51da5804d3d0016619d159da3bd9
Hewan Redie <tewol22@gmail.com>
2021-06-23 16:30:57 -0500

    fixing the text area and changing save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,59 @@
 // Use Moment.js to format the following variables:
 
 
+$(document).ready(function(){
+
 // 1. Current Day display at the top of the calander
-var current = moment().format("MMM D, YYYY");
+var current = moment().format("MMM Do, YYYY");
 $("#currentDay").text(current);
 
 
-var events = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //2. scrolling down should present a timeblocks for business hours
 
-
+    //Done
 
 //3. Each timeblock is color coded to indicate the past, present, or future dates.
 
-var calApperance = function() {
+function calApperance() {
 
+    //gets the current hour
     var currentHour = moment().hour();
 
-    console.log(currentHour);
+    //console.log(currentHour);
 
      $(".row").each(function(){
         var hourElm = parseInt ($(this).attr("id"));
 
         if (hourElm < currentHour){
-            $(this).removeClass(["present", "future"]).addClass("past");
+            $(this).removeClass(["present", "future"]);
+            $(this).addClass("past");
 
         } else if (hourElm === currentHour){
-            $(this).removeClass(["papastst", "future"]).addClass("present");
+            $(this).removeClass(["past", "future"]);
+            $(this).addClass("present");
 
         }else{
 
-            $(this).removeClass(["past", "present"]).addClass("future");
+            $(this).removeClass(["past", "present"]);
+            $(this).addClass("future");
         }
 
     })
-
-   
-
-};
+}
 
 
 //4. Should be able to enter an event by clicking inside the timeblock.
 
+// done
 
 //5. Event is saved in local storage when clicking save button for the timeblock.
+
 
 
 //6. Refresing the page will save the events entered
 
 
-
 calApperance();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
